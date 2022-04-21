52d97313e1fdf2c85d8a718e879da7719bf18cdf
Tsubarah <Tsubarah@hotmail.se>
2022-04-21 20:52:58 +0200

    working on the people's page

--- src/pages/Films.jsx
+++ src/pages/Films.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import StarwarsAPI from "../services/StarwarsAPI"
 
 export default function Films() {
+  const [films, setFilms] = useState()
+  
+
+  const getFilms = async () => {
+    const data = await StarwarsAPI.getFilms()
+    setFilms(data)
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getFilms()
+  }, [])
+
   return (
     <>
       <h1>Films</h1>
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,25 +1,52 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import ListGroup from 'react-bootstrap/ListGroup'
+import { Link } from 'react-router-dom'
 import StarwarsAPI from '../services/StarwarsAPI'
 
 export default function People() {
-
   const [people, setPeople] = useState()
-  const { id } = useParams()
+  const [results, setResults] = useState()
 
   const getPeople = async () => {
     const data = await StarwarsAPI.getPeople()
     setPeople(data)
     console.log(data)
+    console.log(data.results)
   }
+
+  const getNames = () => {
+    const result = people.result
+    const names = result.map(character => {
+      return character.name
+    })
+
+    setResults(names)
+  }
+  
 
   useEffect(() => {
     getPeople()
+
   }, [])
 
   return (
     <>
       <h1>People</h1>
+			{people > 0 && (
+				<ListGroup className="peopleList">
+					{people.map(character =>
+						<ListGroup.Item
+							action
+							as={Link}
+							key={character.id}
+							// to={`/todos/${character.id}`}
+						>
+							{character.name}
+						</ListGroup.Item>
+					)}
+				</ListGroup>
+			)}
+      
     </>
   )
 }
